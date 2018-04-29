4d08ed7c0ffc64c4d1e231acbaed19c9fb9d54b1
hrodberaht <robpszczolka@gmail.com>
2018-04-29 18:03:34 +0200

    add conect to mongodb and worging post and get

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,5 +1,22 @@
 module.exports = function(app,db){
-    app.post('/notes',(req,res) =>{
-        res.json(req.body);
+    app.get('/notes',(req,res)=>{
+        db.collection('notes').find().toArray((err,item)=>{
+            if(err) console.log(err)
+
+            res.send(item);
+        });
     });
+    
+    app.post('/notes',(req,res) =>{
+        
+        const note = {title: req.body.title, text: req.body.body};
+        
+        db.collection('notes').insert(note, (err, result) => {    
+            if (err) { 
+              res.send({ 'error': 'An error has occurred' }); 
+            } else {
+              res.send(result.ops[0]);
+            }
+        });
+});
 }
--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
+
+const db = require('./config/db');
 const port = 8000;
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-require('./app/routes')(app,{});
+MongoClient.connect(db.url,(err,database)=>{
+    if(err) console.log(err)
+    const data = database.db("nodeapi")
+    require('./app/routes')(app,data);
+
+    app.listen(port,() => console.log("Server starts"));
+})
 
 
-app.listen(port,() => console.log("Server starts"));
+
