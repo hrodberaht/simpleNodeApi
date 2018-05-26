82328b2e12e53227be3a8824ded6c7ed5c9c9ca6
hrodberaht <robpszczolka@gmail.com>
2018-05-26 18:13:49 +0200

    add error handling

--- app/routes/error404_routes.js
+++ app/routes/error404_routes.js
@@ -1,5 +1,17 @@
 module.exports = function(app,db){
-    app.use("*",(req,res) => {
-        res.send("404");
+    app.use((req,res,next) => {
+       const err = new Error("Not found");
+       err.status = 404;
+       next(err);
+    })
+
+    app.use((err, req,res, next) => {
+        res.status(err.status || 500);
+        res.json({
+            err: {
+                message: err.message,
+                status: err.status
+            }
+        })
     })
 }
--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const app = express();
 
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(cors());
 
 
