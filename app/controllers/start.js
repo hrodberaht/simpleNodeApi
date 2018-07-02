424fd08b6389b5abe2d1065e569d36ba2e4e01b8
Robert <seedraev@gmail.com>
2018-07-02 13:37:37 +0200

    add controler

---
+++ app/controllers/start.js
@@ -0,0 +1,6 @@
+const start = function start(req,res,next) {
+    res.json({message: "working"});
+}
+
+
+module.exports = { start };
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,14 +1,14 @@
-const noteRoutes = require('./note_routes');
-const error404Routes = require('./error404_routes');
-const userRoutes = require('./user_routes');
+// const noteRoutes = require('./note_routes');
+// const error404Routes = require('./error404_routes');
+// const userRoutes = require('./user_routes');
+
+const start = require("../controllers/start");
 
 
 module.exports = function(app){
 
     app.route("/")
-    .get((req,res,next) => {
-        res.json({message: "ok"});
-    })
+    .get(start);
     // noteRoutes(app,db);
     // userRoutes(app,db);
     // error404Routes(app,db);// this route must be last
