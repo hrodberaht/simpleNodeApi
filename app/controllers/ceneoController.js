9cbaf91a473e9f36eb2d951e6169ddbb27bd4814
hrodberaht <robpszczolka@gmail.com>
2018-07-02 20:48:10 +0200

    adding parser for ceneo xml

--- app/controllers/ceneoController.js
+++ app/controllers/ceneoController.js
@@ -1,5 +1,18 @@
+const fs = require("fs");
+const request = require("request");
+const xml2js = require("xml2js");
+const parser = new xml2js.Parser();
+
 const ceneo = function parseXMLtoCeneo(req,res,next){
-    res.json({messege: "ceneo"});
+    const xml = "http://zefi.pl/console/integration/execute/name/CeneoV2";
+    request.get(xml,function(err,response,body) {
+        if(err)
+            throw err;
+        parser.parseString(body,function(err,result){
+            res.send(result.offers.o);
+        })
+       
+    });
 }
 
 
